016dc2915c5d8d20b240cbef36f8d1176e3aea20
gfauceglia <98237396+gfauceglia@users.noreply.github.com>
2023-06-01 20:07:07 -0300

    finish ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(initial, final) {
+  if (!Number.isInteger(initial) || !Number.isInteger(final) || initial < 0 || final < 0) {
+    return 'ERROR'
+  } else if (initial > final) {
+    let aux = initial;
+    initial = final;
+    final = aux;
+  }
+  let sum = 0;
+  for (initial; initial <= final; initial++) {
+    sum += initial;
+  }
+  return sum
 };
 
 // Do not edit below this line
